dcd82fe27eafed9cfc19acb3d921b26153baac40
yankiweiss <yakovw2706@gmail.com>
2025-05-21 10:54:14 -0400

    update

--- public/item.js
+++ public/item.js
@@ -22,3 +22,47 @@ fetch(`https://full-stack-plumbing-ecomerce.vercel.app/items/${encodeURIComponen
   });
 
 
+function getQueryParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+const itemDisplayedID = getQueryParam('id'); // e.g. from URL
+
+const items = document.getElementById('items')
+
+fetch(`https://full-stack-plumbing-ecomerce.vercel.app/items/${encodeURIComponent(itemDisplayedID)}`)
+  .then(res => {
+    if (!res.ok) throw new Error(`Server error: ${res.status}`);
+    return res.json();
+  }).then((data) => displayData(data))
+
+  .catch(err => {
+    console.error(err);
+    document.getElementById('item-name').textContent = 'Failed to load item';
+  });
+
+function displayData(data) {
+
+  const div = document.createElement('div')
+  div.className = 'item';
+
+  div.innerHTML = `
+    
+    
+    
+    <img src="${data.image}">
+    
+    <h1>${data.name}</h1>
+    
+    <h4>Price: ${data.price}</h4>
+    
+    <h5>Description: ${data.description}</h5>`
+
+  items.appendChild(div)
+}
+
+
+
+
+
