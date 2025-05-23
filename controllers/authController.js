869bc356acedbd3fa57f5732de3732c9587d3e23
yankiweiss <yakovw2706@gmail.com>
2025-05-23 12:37:26 -0400

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,72 +1,25 @@
 const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+    users: require('../model/users.json'),
+    setUsers : function (data) {this.users = data}
+}
 
-const bcrypt = require("bcryptjs");
-
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-
-const fsPromises = require("fs").promises;
-
-const path = require("path");
+const bcrypt = require('bcryptjs');
 
 const handleLogin = async (req, res) => {
-  const { user, pwd } = req.body;
+    const {user, pwd } = req.body;
+    if(!user|| !pwd) return res.status(400).json({"message" : "Username and Password are Required!"})
 
-  if (!user || !pwd)
-    return res
-      .status(400)
-      .json({ message: "Username and Password are required!" });
+        const foundUser = usersDB.users.find(person => person.username === user)
 
-  const foundUser = usersDB.users.find((person) => person.username === user);
+        if(!foundUser) return res.sendStatus(401);
 
-  if (!foundUser)
-    return res
-      .status(401)
-      .json({ message: "Your Username has not been found!" });
+        const match = await bcrypt.compare(pwd, foundUser.password);
 
-  const match = await bcrypt.compare(pwd, foundUser.password);
+        if(match) {
+            res.json({'message': `user ${user} is logged in!`})
+        }else {
+            res.status(401)
+        }
+}
 
-  if (match) {
-    const accessToken = jwt.sign(
-      { username: foundUser.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "30s" }
-    );
-
-    const refreshToken = jwt.sign(
-      { username: foundUser.username },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-
-    const otherUsers = usersDB.users.filter(
-      (person) => person.username !== foundUser.username
-    );
-
-    const currentUser = { ...foundUser, refreshToken };
-
-    usersDB.setUsers([...otherUsers, currentUser]);
-
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users)
-    );
-    res.cookie('jwt', refreshToken, {httpOnly: true, maxAge: 24 * 60 * 60 * 1000})
-    res.json({
-      message: `${user} is now successfully logged in!, ${accessToken}`
-    });
-  } else {
-    res
-      .status(401)
-      .json({ message: "your password did not match our records" });
-  }
-};
-
-module.exports = {
-  handleLogin,
-};
+module.exports = {handleLogin}
